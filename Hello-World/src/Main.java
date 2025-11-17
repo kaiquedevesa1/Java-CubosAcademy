988f9d2ab9f65e9ca3c9260dd8edd239badf2f7f
Kaique Devesa <118691941+kaiquedevesa1@users.noreply.github.com>
2025-11-17 19:49:45 -0300

    Constante e variável no java

---
+++ Hello-World/src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        String txt = "Kaique Devesa";
+
+        System.out.printf(txt);
+
+        txt = "Cubos Academy";
+
+        System.out.printf(txt);
+
+        final String txt2 = "Minha primeira constante";
+
+        }
+    }
