8cbc2801fa6d66e4aa829708be78949563073c03
Kaique Devesa <118691941+kaiquedevesa1@users.noreply.github.com>
2025-11-22 21:06:36 -0300

    Add files via upload

--- Hello-World/src/Main.java
+++ Hello-World/src/Main.java
@@ -4,14 +4,31 @@ public class Main {
     public static void main(String[] args) {
 
         String txt = "Kaique Devesa";
-
+            //imprime
         System.out.printf(txt);
 
+            //sobrescreve variavel
         txt = "Cubos Academy";
 
+            //imprime
         System.out.printf(txt);
 
+            //constante
         final String txt2 = "Minha primeira constante";
+
+        //imprime constante
+        System.out.println(txt2);
+
+        //Números
+        //números inteiros
+        int numeroInteiro = 12;
+        System.out.println(numeroInteiro);
+        //números decimais
+        double numeroDecimal = 12.03441;
+        System.out.println(numeroDecimal);
+        //Lógico
+        boolean verdadeiroOuFalso = true;
+        System.out.println(verdadeiroOuFalso);
 
         }
     }
