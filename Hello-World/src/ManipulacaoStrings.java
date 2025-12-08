7b93e4ce41809ce8c903812a5add9bb2cb639c9d
Kaique Devesa <118691941+kaiquedevesa1@users.noreply.github.com>
2025-12-07 21:42:57 -0300

    startsWith endWith

--- Hello-World/src/ManipulacaoStrings.java
+++ Hello-World/src/ManipulacaoStrings.java
@@ -1,9 +1,21 @@
 public class ManipulacaoStrings {
 
     public static void main(String[] args) {
-        String variavel1 = "Kaque Devesa";
+
         double numero1 = 13.5;
         String variavelConvertida = String.valueOf(numero1);
         System.out.println(variavelConvertida.charAt(1));
+
+        //verifica se a string comeca com a ocorrencia informada
+
+        String variavel1 = "Kaique Devesa";
+        boolean resultado1 = variavel1.startsWith("Devesa");
+        boolean resultado2 = variavel1.startsWith("Kaique Devesa", 0);
+        System.out.println(resultado2);
+        // verifica se a string termina com a ocorrencia informada
+        boolean resultado3 = variavel1.endsWith("a");
+        System.out.println(resultado3);
+
+
     }
 }
