946a03a7a9b92e0519c45a3d12d4f5756a73d38a
Kaique Devesa <118691941+kaiquedevesa1@users.noreply.github.com>
2025-12-20 22:53:30 -0300

    arrayMultimensional

--- Hello-World/src/ManipulacaoArrays.java
+++ Hello-World/src/ManipulacaoArrays.java
@@ -2,9 +2,9 @@ public class ManipulacaoArrays {
 
     public static void main(String[] args){
         int[] array = {1, 234, 987};
-        /*int []array2 = {1, 234, 987};
+        int []array2 = {1, 234, 987};
         int array3[] = {1, 234, 987};
-        System.out.println(array[2]);*/
+        System.out.println(array[2]);
 
         int[] array4 = new int[5];
         array4[0] = 2000;
@@ -15,5 +15,22 @@ public class ManipulacaoArrays {
         array4[1] = 1234;
 
         System.out.println(array4[1]);
+
+
+        //ArrayMultimensional
+
+        String [][] arrayMultimensional = {{"Pedro" , "Joao"}, {"Maria", "Antonia"}};
+        System.out.println(arrayMultimensional[1][1]);
+        System.out.println(arrayMultimensional[0][1]);
+
+        int[][] arrayMultimensional2 = new int [2][2];
+        arrayMultimensional2 [0][0] = 1;
+        arrayMultimensional2 [0][1] = 2;
+        arrayMultimensional2 [1][0] = 3;
+        arrayMultimensional2 [1][1] = 4;
+
+
+
+        System.out.println(arrayMultimensional2[1][2]);
     }
 }
