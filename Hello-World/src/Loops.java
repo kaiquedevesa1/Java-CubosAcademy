5dcf35c2251947a05dcdd48e4cc68c518767dcb2
Kaique Devesa <118691941+kaiquedevesa1@users.noreply.github.com>
2025-12-21 20:28:36 -0300

    loops

---
+++ Hello-World/src/Loops.java
@@ -0,0 +1,12 @@
+public class Loops {
+
+    public static  void main(String[] args){
+        int[] arraySimples = {1, 2 , 3, 4};
+
+        for(int i = 0; i < arraySimples.length; i++){
+            System.out.println(arraySimples[i]);
+        }
+
+
+    }
+}
