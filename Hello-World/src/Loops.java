489682955cfc20c9987d731fdda40605834021d6
Kaique Devesa <118691941+kaiquedevesa1@users.noreply.github.com>
2025-12-21 20:41:55 -0300

    foreach

--- Hello-World/src/Loops.java
+++ Hello-World/src/Loops.java
@@ -3,8 +3,12 @@ public class Loops {
     public static  void main(String[] args){
         int[] arraySimples = {1, 2 , 3, 4};
 
-        for(int i = 0; i < arraySimples.length; i++){
+        /*for(int i = 0; i < arraySimples.length; i++){
             System.out.println(arraySimples[i]);
+        }*/
+
+        for (int numero : arraySimples){
+            System.out.println(numero);
         }
 
 
