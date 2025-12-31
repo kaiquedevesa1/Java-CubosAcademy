ac46119ce4f379e36f0224668b8231ef1bb69d2b
Kaique Devesa <118691941+kaiquedevesa1@users.noreply.github.com>
2025-12-30 21:24:51 -0300

    construtor

--- Orientacao-objeto/src/Main.java
+++ Orientacao-objeto/src/Main.java
@@ -2,15 +2,16 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-       carro fusca = new carro();
+      /* carro fusca = new carro();
        fusca.modelo = "fusca";
        fusca.marca = "Volkswagen";
        fusca.cor = "preto";
-       fusca.ano= 1978;
-
-        System.out.println(fusca);
+       fusca.ano= 1978; */
 
 
+        carro gol = new carro("Gol", "Volkswagen", "branco", 2010);
+        gol.ano = 2012;
+
+        System.out.println(gol.ano);
     }
 }
--- Orientacao-objeto/src/carro.java
+++ Orientacao-objeto/src/carro.java
@@ -8,4 +8,12 @@ public class carro {
     public String cor;
 
     public int ano;
+
+
+    public carro(String modelo, String marca ,String cor , int ano){
+    this.modelo = modelo;
+    this.marca = marca;
+    this.cor = cor;
+    this.ano = ano;
+    }
 }
