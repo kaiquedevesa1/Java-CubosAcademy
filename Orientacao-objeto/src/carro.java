4ba259cdd36853f5a121615c8b0511e9d3e1a29c
Kaique Devesa <118691941+kaiquedevesa1@users.noreply.github.com>
2026-01-06 23:45:33 -0300

    métodos

--- Orientacao-objeto/src/carro.java
+++ Orientacao-objeto/src/carro.java
@@ -9,11 +9,50 @@ public class carro {
 
     public int ano;
 
+    public boolean ligado;
+    public int aceleracao;
 
     public carro(String modelo, String marca ,String cor , int ano){
     this.modelo = modelo;
     this.marca = marca;
     this.cor = cor;
     this.ano = ano;
+    this.ligado = false;
+    this.aceleracao = 0;
+
+    }
+
+    //usase o void caso nao queira retornar nada
+    public void ligar(){
+    if (this.ligado){
+        return;
+    }
+
+    this.ligado = true;
+
+    }
+
+
+    public void desligado(){
+        if (!this.ligado){
+            return;
+        }
+
+        this.ligado = false;
+
+    }
+
+    public void acelerrar(String tipoAceleracao){
+        switch (tipoAceleracao){
+            case "forte":
+                this.aceleracao += 1000;
+                break;
+            case "fraca":
+                this.aceleracao += 500;
+                break;
+            default:
+                this.aceleracao += 750;
+                break;
+        }
     }
 }
