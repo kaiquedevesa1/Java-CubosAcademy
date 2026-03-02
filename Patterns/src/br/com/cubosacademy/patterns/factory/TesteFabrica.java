cbe2cbbca081893d05b29d63408b076e04f593ab
Kaique Devesa <118691941+kaiquedevesa1@users.noreply.github.com>
2026-03-02 20:49:52 -0300

    Factory

--- Patterns/src/br/com/cubosacademy/patterns/factory/TesteFabrica.java
+++ Patterns/src/br/com/cubosacademy/patterns/factory/TesteFabrica.java
@@ -2,6 +2,7 @@ package br.com.cubosacademy.patterns.factory;
 
 public class TesteFabrica {
     public static void main(String[] args) {
-
+        Moeda moeda = MoedaFabrica.getInstance("CA");
+        System.out.println(moeda.getPrefixo());
     }
 }
