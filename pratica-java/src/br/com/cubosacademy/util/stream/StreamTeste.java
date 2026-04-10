43b0550136e69dd961aaf5a8f6a6113a6d754147
Kaique Devesa <118691941+kaiquedevesa1@users.noreply.github.com>
2026-04-09 21:44:41 -0300

    stream

---
+++ pratica-java/src/br/com/cubosacademy/util/stream/StreamTeste.java
@@ -0,0 +1,20 @@
+package br.com.cubosacademy.util.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamTeste {
+    public static void main(String[] args) {
+        List<String>  nomes = new ArrayList<String>();
+        nomes.add("Joao");
+        nomes.add("Maria");
+        nomes.add("Kaique");
+
+        //System.out.println(nomes.stream().count());
+
+        Stream<String> nomes2 = nomes.stream();
+        Stream<String> result = nomes2.filter (nome -> nome.equals("Kaique"));
+        System.out.println(result.toList());
+    }
+}
