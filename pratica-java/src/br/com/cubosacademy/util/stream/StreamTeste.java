1475d5bd1297ad881c00d84e089393bbbbe0a90b
Kaique Devesa <118691941+kaiquedevesa1@users.noreply.github.com>
2026-04-20 20:53:25 -0300

    Stream map and filter

--- pratica-java/src/br/com/cubosacademy/util/stream/StreamTeste.java
+++ pratica-java/src/br/com/cubosacademy/util/stream/StreamTeste.java
@@ -15,6 +15,27 @@ public class StreamTeste {
 
         Stream<String> nomes2 = nomes.stream();
         Stream<String> result = nomes2.filter (nome -> nome.equals("Kaique"));
-        System.out.println(result.toList());
+       // System.out.println(result.toList());
+
+
+        Pessoa kaique = new Pessoa ("kaique", "devesa");
+        Pessoa pedro = new Pessoa ("pedro", "andrade");
+        Pessoa ana = new Pessoa ("ana", "oliveira");
+
+        List<Pessoa> usuarios = new ArrayList<Pessoa>();
+
+        usuarios.add(kaique);
+        usuarios.add(pedro);
+        usuarios.add(ana);
+
+
+        Stream<Pessoa> usuariosStream = usuarios.stream();
+        Stream<String> resultado = usuariosStream.map( usuario -> {
+            return usuario.getNome() + " " + usuario.getSobrenome();
+        });
+
+        System.out.println(resultado.toList());
     }
+
+
 }
