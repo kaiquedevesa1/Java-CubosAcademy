2487c343f2ea4238c0c6a30ac424cd4fd8de8b78
Kaique Devesa <kaikedaeves@hotmail.com>
2026-05-09 19:12:37 -0300

    gerenics

---
+++ pratica-java/src/br/com/cubosacademy/util/generics/GenericsTeste.java
@@ -0,0 +1,15 @@
+package br.com.cubosacademy.util.generics;
+
+public class GenericsTeste {
+
+    public static void main(String[] args) {
+        Objeto<String> nome = new Objeto<String>("Pedro");
+        Objeto<Integer> num = new Objeto<Integer>(123);
+        Objeto<Boolean> verdadeiro = new Objeto<Boolean>(true);
+
+
+
+
+
+    }
+}
---
+++ pratica-java/src/br/com/cubosacademy/util/generics/Objeto.java
@@ -0,0 +1,19 @@
+package br.com.cubosacademy.util.generics;
+
+public class Objeto <O>{
+
+    private O nome;
+
+    public Objeto(O nome) {
+        this.nome = nome;
+    }
+
+
+    public O getNome() {
+        return nome;
+    }
+
+    public void setNome(O nome) {
+        this.nome = nome;
+    }
+}
